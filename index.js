ec17561245a6b7b8f3d76e90a4007c199cc93ee3
PENG <46526272+BMW3888@users.noreply.github.com>
2021-07-04 13:48:24 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+$ git clone https://github.com/YOUR-USERNAME/YOUR-REPOSITORY.git
+$ cd YOUR-REPOSITORY
+alert("Hello, World!");
+$ npm init
+  ...
+  package name: @BMW3888/xcobe
+  ...
+  test command: exit 0
+  ...    
+  $ npm install
+$ git add index.js package.json package-lock.json
+$ git commit -m "initialize npm package"
+$ git push
